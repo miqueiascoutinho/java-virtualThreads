5f43a303825c56d8d63c18a2c0915491d40c2cd3
Miqueias Coutinho <miqueias.sc@hotmail.com>
2025-06-29 20:51:51 -0300

    Adicionando endpoint /threads que recebe o parâmetro timeout, para facilitar nos testes

--- src/main/java/br/com/miqueias/virtualThread/Controller.java
+++ src/main/java/br/com/miqueias/virtualThread/Controller.java
@@ -3,8 +3,11 @@ package br.com.miqueias.virtualThread;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @RestController
@@ -12,9 +15,23 @@ import java.time.LocalDateTime;
 public class Controller {
     @GetMapping
     public ResponseEntity<Status> virtual() throws InterruptedException {
-        System.out.println("Thread -> " + Thread.currentThread().getName());
-        Thread.sleep(2000L);
-        return ResponseEntity.ok(new Status());
+        Instant inicio = Instant.now();
+
+        Thread.sleep(200L);
+        var result = new Status();
+        Instant fim = Instant.now();
+        System.out.println("Thread -> " + Thread.currentThread().getName() + " - Duração: " + Duration.between(inicio, fim).toMillis() + "ms");
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/thread")
+    public ResponseEntity<Status> virtualThreadComTimeout(@RequestParam Long timeout) throws InterruptedException {
+        Instant inicio = Instant.now();
+        Thread.sleep(timeout);
+        var result = new Status();
+        Instant fim = Instant.now();
+        System.out.println("Thread -> " + Thread.currentThread().getName() + " - Duração: " + Duration.between(inicio, fim).toMillis() + "ms");
+        return ResponseEntity.ok(result);
     }
 }
 record Status(LocalDateTime time, String status){
